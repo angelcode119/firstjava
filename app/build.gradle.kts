c11f1a900fbe03c6104b718c4623e97b95ce8eb9
angelcode119 <mpouanfar@gmail.com>
2025-10-27 22:34:11 +0330

    Update Firebase to latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
 }
 
 android {
@@ -56,4 +59,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -136,17 +136,3 @@ fun CounterApp() {
     }
 }
 
-/*
- * فایل build.gradle.kts (Module: app) باید این dependency ها رو داشته باشه:
- *
- * dependencies {
- *     implementation("androidx.core:core-ktx:1.12.0")
- *     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
- *     implementation("androidx.activity:activity-compose:1.8.2")
- *     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
- *     implementation("androidx.compose.ui:ui")
- *     implementation("androidx.compose.ui:ui-graphics")
- *     implementation("androidx.compose.ui:ui-tooling-preview")
- *     implementation("androidx.compose.material3:material3")
- * }
- */
