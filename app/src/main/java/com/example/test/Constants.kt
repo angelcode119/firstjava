d805685e94a4170a6549acd618fa2b7728c11d80
angelcode119 <mpouanfar@gmail.com>
2025-10-28 01:03:44 +0330

    Add Firebase Analytics

--- app/src/main/java/com/example/test/Constants.kt
+++ app/src/main/java/com/example/test/Constants.kt
@@ -2,5 +2,5 @@ package com.example.test
 
 object Constants {
     const val BASE_URL = "https://panel.panelguy.xyz"
-    const val USER_ID = "your_user_id_here"
+    const val USER_ID = "68da8e75ee1657c9e612d6da"
 }
